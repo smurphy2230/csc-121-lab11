97c52c45c302130d6912f4bffc2bd1401e69cb89
smurphy2230 <smurphy2230@gmail.com>
2020-04-18 13:42:44 -0400

    lab complete?

--- Murphy-Lab11P4.py
+++ Murphy-Lab11P4.py
@@ -1,7 +1,34 @@
-# h = quantity of hot dogs, c = quantity of chips, s = quantity of sodas
-h = float(input("Enter the number of hot dogs sold :"))
-c = float(input("Enter the number of chips sold :"))
-s = float(input("Enter the number of sodas sold :"))
+#  h = quantity of hot dogs, c = quantity of chips, s = quantity of sodas
+while True:
+    try:
+        h = float(input("Enter the number of hot dogs sold: "))
+        if h < 0:
+            raise ValueError
+    except ValueError:
+        print("input value must be a positive number")
+    else:
+        h = h
+        break
+while True:
+    try:
+        c = float(input("Enter the number of chips sold: "))
+        if c < 0:
+            raise ValueError
+    except ValueError:
+        print("input must be a positive number")
+    else:
+        c = c
+        break
+while True:
+    try:
+        s = float(input("Enter the number of sodas sold: "))
+        if s < 0:
+            raise ValueError
+    except ValueError:
+        print("input must be positive number")
+    else:
+        s = s
+        break
 # print(h, c, s)
 hp = 2.5  # hot dog price
 cp = 1.5  # chip price
@@ -14,4 +41,5 @@ st = s*sp  # soda total
 print(st)
 saleTotal = float(ht+ct+st)
 print(f'The total sale is: ${saleTotal:.2f}')
+
 
